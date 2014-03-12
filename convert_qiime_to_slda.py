34f315368f744abcb75b2450651c9ae140f238a5
samfway <samfway@gmail.com>
2014-03-12 11:03:18 -0600

    Added file creation

--- convert_qiime_to_slda.py
+++ convert_qiime_to_slda.py
@@ -3,6 +3,7 @@ from numpy import array
 import argparse
 from argparse import RawTextHelpFormatter
 from ml_utils.parse import load_dataset 
+from ml_utils.util import convert_labels_to_int
 
 def interface():
     args = argparse.ArgumentParser(
@@ -29,15 +30,29 @@ def create_slda_dataset(data_matrix, sample_ids, labels, output_prefix):
     """ Reformat matrix+labels for the SLDA C implementation.  
         An extra file is created to track sample_ids but is not needed by SLDA
     """ 
-    
     # 1)  Create labels file: one label per line 
-    unique_labels = list(set(labels))
-    print unique_labels
-    exit()
+    unique_labels, label_indices = convert_labels_to_int(labels)
+    output_name = output_prefix + 'labels.txt'
+    output = open(output_name, 'w')
+    output.write('\n'.join([str(l) for l in label_indices]))
+    output.close()
 
-    print len(sample_ids)
-    print data_matrix.shape
-    print labels
+    # 2) Create sample id file: one per line (NOT NEEDED BY SLDA)
+    output_name = output_prefix + 'sample_ids.txt'
+    output = open(output_name, 'w')
+    output.write('\n'.join(sample_ids))
+    output.close() 
+    
+    # 3) Create data file for SLDA Format: 
+    #    <M> <term_1>:<count> <term_2>:<count> ... <term_N>:<count>
+    # Where M is the number of unique, non-zero elements in the matrix
+    output_name = output_prefix + 'data.txt' 
+    output = open(output_name, 'w')
+    N = data_matrix.shape[1] 
+    for row in data_matrix:
+        to_write = [ '%d:%d'%(k, row[k]) for k in xrange(N) if row[k] > 0 ] 
+        output.write('%d %s\n' % (len(to_write), to_write))
+    output.close()
 
 if __name__=="__main__":
     args = interface()
